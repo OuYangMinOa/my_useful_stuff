609f3501545d8b1f17edb2fa36cdb2ed31f14e89
ouyang-company <wesley91345@gmail.com>
2025-05-15 03:29:42 +0000

    update

--- auto_install/install_ou_nvim.sh
+++ auto_install/install_ou_nvim.sh
@@ -6,7 +6,6 @@ apt-get install -y neovim
 git config --global http.sslbackend gnutls schannel
 curl -o- https://fnm.vercel.app/install | bash
 /root/.local/share/fnm/fnm install 22
-git config --global http.sslbackend gnutls
 mkdir ~/.config
 cd ~/.config
 git clone https://github.com/OuYangMinOa/neovim_setup ~/.config/nvim
