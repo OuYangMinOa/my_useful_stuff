5e8910880e47b202cdf8092b866b078bdedc5ae3
ouyang-company <wesley91345@gmail.com>
2025-05-15 03:31:16 +0000

    update

--- auto_install/install_zellij.sh
+++ auto_install/install_zellij.sh
@@ -1,4 +1,4 @@
-git config --global http.sslbackend schannel
+git config --global http.sslbackend gnutls
 curl https://sh.rustup.rs -sSf | bash -s -- -y
 echo 'source $HOME/.cargo/env' >> $HOME/.bashrc
 PATH="/root/.cargo/bin:${PATH}"
